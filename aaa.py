a45c05d4b0d6fb3588114cef2358316910228cae
shjwhejwbwbajw528158 <119314040+shjwhejwbwbajw528158@users.noreply.github.com>
2022-11-28 18:43:50 +0600

    Create aaa.py

---
+++ aaa.py
@@ -0,0 +1,19 @@
+from telegram.ext import *
+
+
+
+def st(u,c):
+    u.message.reply_text("Hello Bro")
+
+
+
+
+
+u = Updater("5938418108:AAHymtX7mJfhDZQI97FkASRpxdnz2c2Q-4Y",use_context=True)
+
+
+
+u.dispatcher.add_handler(CommandHandler("start",st))
+
+u.start_polling()
+u.idle()
